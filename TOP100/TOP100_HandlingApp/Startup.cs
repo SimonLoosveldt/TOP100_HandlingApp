c97da3099da856df7cb9946f0f1bd253cd6ea609
Michel Michels <michel.michels.jr@gmail.com>
2020-03-07 16:31:38 +0100

    Fake database added

--- TOP100/TOP100_HandlingApp/Startup.cs
+++ TOP100/TOP100_HandlingApp/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLibrary;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace TOP100_HandlingApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<IUserData, UserData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
